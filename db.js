473309e32e811d3a18d41e75365047048edc8935
Jasurbek <jasurbekbagibekov@gmail.com>
2026-03-07 00:03:38 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  // Railway'dagi DATABASE_URL o'zgaruvchisidan hamma ma'lumotni oladi
+  connectionString: process.env.DATABASE_URL, 
   ssl: {
     rejectUnauthorized: false
   }
