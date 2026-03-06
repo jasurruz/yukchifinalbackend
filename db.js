cfdb16af54a7d780e9ed1cb0adb7a5235a89fbd8
Jasurbek <jasurbekbagibekov@gmail.com>
2026-03-07 00:40:47 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
+// db.js
 const { Pool } = require("pg");
+
 const pool = new Pool({
+  // Railway o'zi taqdim etgan DATABASE_URL ni ishlatadi
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 module.exports = pool;
